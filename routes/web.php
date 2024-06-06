d40419cd813aedb68581a0631c7fa1d17df32060
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-06 15:03:50 +0200

    add: data in routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $greetings = "Hello World!";
+    $help = "Mi è arrivato il letto da montare e sulle recensioni dicono che servono almeno due persone, ma sono da solo e non mi arrenderò";
+    $motivation = "ricordate sempre che chi si estranea dalla lotta è un gran figlio di una ...";
+    $credits = "cit. il mio prof di italiano in seconda media ai nazionali di calcetto con la scuola";
+
+    $data = [
+        'greetings' => $greetings,
+        'help' => $help,
+        'motivation' => $motivation,
+        'credits' => $credits,
+    ];
+    return view('welcome', $data);
 });
